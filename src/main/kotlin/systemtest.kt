b6b27d7c6f67307aadb8247924d968faf56bb668
0finderfail <ilyalvov2007@gmail.com>
2022-06-26 00:33:23 +0300

    Update main control fix Update systemtest.kt

--- src/main/kotlin/systemtest.kt
+++ src/main/kotlin/systemtest.kt
@@ -5,4 +5,10 @@ import kotlin.*
         println("Debug: Kotlin")
         println("Debug: Java 1.8")
         println("Debug: Build on IntelliJ IDEA build tools")
+        var systemtest_1 = 1
+        var systemtest_3 = systemtest_1 + systemtest_1 * systemtest_1
+        var systemtest_2 = true
+        println(systemtest_1)
+        println(systemtest_2)
+        println(systemtest_3)
     }
